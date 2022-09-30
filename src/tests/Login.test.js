65215416dd777cbcd898ffef572de3123a639418
Nassim <nassim.bouyacoub@gmail.com>
2022-09-30 15:00:49 +0200

    adding tests

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,14 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Login from '../Components/Login'
 import React from 'react'
 import { Provider } from "react-redux";
-import {store} from '../index'
+import { store } from '../index'
+import { MemoryRouter } from "react-router-dom";
 
-test("testing login component", () => {
-    var component = render(
-        <Provider store={store}>
-            <Login />
-        </Provider>
+describe("testing login component", () => {
+    render(
+        <MemoryRouter>
+            <Provider store={store}>
+                <Login />
+            </Provider>
+        </MemoryRouter>
     )
-    expect(component).toMatchSnapshot()
+    it("Compare to snapshot", () => {
+        expect(screen).toMatchSnapshot()
+    })
+    it("Verify that user is Logged", async () => {
+        // console.log(component)
+        const test = screen.getByTestId("authUser")
+        console.log(test)
+        // expect(test).toBeInTheDocument()
+        // fireEvent.click(log)
+        // expect(props.authedUser).not.toBeNull()
+    })
 })
